16ae8211d5b89ea11569791619fa6e228726ab68
Waltwang441 <31419755+Waltwang441@users.noreply.github.com>
2022-12-11 01:49:05 -0500

    train and loss func update

--- train.py
+++ train.py
@@ -1,17 +1,17 @@
 import torch
 import torch.nn as nn
-import torch.optim as optim
 from tqdm import tqdm
 from augmentations import augment
-from utils import pairwise_sim, NTXent
+from utils import SimCLR_Loss
+
 
 # credit to https://www.zhihu.com/question/523869554 for training function template
 def train(net, device, batch_size, train_loader, val_loader, num_epoch, lr, optim='sgd', scheduler='None', temp=0.1):
     net.to(device)
     if optim == 'sgd':
-        optimizer = optim.SGD((param for param in net.parameters() if param.requires_grad), lr=lr, weight_decay=0)
+        optimizer = torch.optim.SGD((param for param in net.parameters() if param.requires_grad), lr=lr, weight_decay=0)
     elif optim == 'adam':
-        optimizer = optim.Adam((param for param in net.parameters() if param.requires_grad), lr=lr, weight_decay=0)
+        optimizer = torch.optim.Adam((param for param in net.parameters() if param.requires_grad), lr=lr, weight_decay=0)
     train_loss_list = []
     train_acc_list = []
     val_loss_list = []
@@ -27,33 +27,25 @@ def train(net, device, batch_size, train_loader, val_loader, num_epoch, lr, opti
         train_loss = 0
         for batch in tqdm(train_loader):
             image, label = batch
+            actual_batch_size = image.shape[0]  # taking care of the last batch in case it cannot be fully divided
+            loss_func = SimCLR_Loss(actual_batch_size, temp)
             image, label = image.to(device), label.to(device)
-            # perform augmentation & compute cosine similarities
-            aug1 = augment(image)
-            aug2 = augment(image)
-            z1 = net(aug1, inference=False) # batch_size x 2048
-            z2 = net(aug2, inference=False) # batch_size x 2048
-            z_mat = torch.cat((z1, z2), 0)
-            sim_mat = pairwise_sim(z_mat)
-            # compute loss
-            loss = 0
-            for i in range(batch_size):
-                loss += NTXent(2*i, 2*i+1, sim_mat, temp) + NTXent(2*i+1, 2*i, sim_mat, temp)
-            loss /= 2 * batch_size
-            train_loss += loss
+            # perform augmentation & compute training loss
+            aug1, aug2 = augment(image, augment_twice=True)
+            h1, z1 = net(aug1)  # h1: batch_size x 10; z1: batch_size x 2048
+            h2, z2 = net(aug2)
+            loss = loss_func(z1, z2)
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
+            train_loss += loss
             # compute training acc
-            net.eval()
-            with torch.no_grad():
-                pred = net(image, inference=True)
-            train_acc += torch.sum(pred == label).item() / batch_size
+            pred, _ = net(image)
+            train_acc += torch.sum(pred == label).item() / actual_batch_size
         # print out average training loss and acc over all minibatches
         print("Epoch: {}, Training Loss: {}, Training Acc: {}".format(epoch, train_loss / len(train_loader), train_acc / len(train_loader)))
         if best_tr_acc < train_acc / len(train_loader):
             best_tr_acc = train_acc / len(train_loader)
-            torch.save(net.state_dict(), model_checkpoint)
         print('Best Training Acc: ', best_tr_acc)
         train_acc_list.append(train_acc / len(train_loader))
         train_loss_list.append(train_loss / len(train_loader))
@@ -66,13 +58,18 @@ def train(net, device, batch_size, train_loader, val_loader, num_epoch, lr, opti
         with torch.no_grad():
             for image, label in val_loader:
                 image, label = image.to(device), label.to(device)
-                pred = net(image, inference=True)
+                actual_batch_size = image.shape[0]
+                pred, _ = net(image)
                 loss_func = nn.CrossEntropyLoss()
                 val_loss += loss_func(label, pred)
-                val_acc += torch.sum(pred == label).item() / batch_size
+                val_acc += torch.sum(pred == label).item() / actual_batch_size
             print("Validation Loss: {}, Validation Acc: {}".format(val_loss / len(val_loader), val_acc / len(val_loader)))
             if best_val_acc < val_acc / len(val_loader):
                 best_val_acc = val_acc / len(val_loader)
+                torch.save(net.state_dict(), model_checkpoint)
+                print('Saving model...')
             print('Best Val Acc: ', best_val_acc)
             val_acc_list.append(val_acc / len(val_loader))
             val_loss_list.append(val_loss / len(val_loader))
+
+    return train_loss_list, train_acc_list, val_loss_list, val_acc_list
--- utils.py
+++ utils.py
@@ -4,24 +4,41 @@ import scipy
 from torch.nn import functional as F
 
 
-def pairwise_sim(z_mat):
-    # compute pairwise cosine similarity between every pair of z's
-    # z_mat: all z vectors obtained from projector, with shape batch_dim x 2048
-    # batch_dim = 2N, 2 * the original batch size
-    # credit to https://stackoverflow.com/questions/60467264/pairwise-similarity-matrix-between-a-set-of-vectors-in-pytorch
-    # for computing the pairwise similarities
-    batch_dim = z_mat.size()[0]
-    sim_mat = F.cosine_similarity(z_mat[None, :, :], z_mat[:, None, :], dim=-1)
-    assert sim_mat.shape == torch.Size([batch_dim, batch_dim])
-    return sim_mat
+class SimCLR_Loss(nn.module):
 
+    def __init__(self, batch_size, temp):
+        super(SimCLR_Loss, self).__init__()
+        self.batch_size = batch_size
+        self.temp = temp
 
-def NTXent(zi, zj, sim_mat, temp):
-    # compute the loss functions for simCLR
-    # zi, zj: idx of z vectors
-    # sim_mat: pairwise similarity matrix
-    # temp: tempature scaling, hyperparameter
-    numerator = torch.exp(sim_mat[zi, zj] / temp)
-    denom = torch.sum(torch.exp(sim_mat[zi] / temp)) - torch.exp(torch.tensor(1) / temp)
-    return -torch.log(numerator / denom)
+    def sim_all_pairs(self, z1, z2):
+        # compute the cosine similarity matrix between every pair of z's
+        # z1, z2: z vectors obtained from different augmentations, batch_size x 2048
+        # credit to https://stackoverflow.com/questions/60467264/pairwise-similarity-matrix-between-a-set-of-vectors-in-pytorch
+        # for computing the pairwise similarities
+        # could be further optimized given that this is a symmetric matrix with diagonal values=1
+        # only need to store the upper/lower triangle matrix if batchsize gets huge
+        z_mat = torch.cat((z1, z2), 0)
+        assert z_mat.shape == torch.Size([2 * self.batch_size, 2048])
+        sim_mat = F.cosine_similarity(z_mat[None, :, :], z_mat[:, None, :], dim=-1)
+        assert sim_mat.shape == torch.Size([2 * self.batch_size, 2 * self.batch_size])
+        return sim_mat
+
+    def sim_pos_pairs(self, z1, z2):
+        # compute the pairwise similarity between every positive pairs
+        # z1, z2: z vectors obtained from different augmentations, batch_size x 2048
+        pair_sim = F.cosine_similarity(z1, z2, dim=1)
+        assert pair_sim.shape == torch.Size([self.batch_size])
+        return pair_sim
+
+    def forward(self, z1, z2):
+        # compute the NTXent loss, vectorized
+        pos_pairs = torch.exp(self.sim_pos_pairs(z1, z2) / self.temp) # batch_size
+        assert pos_pairs.shape == torch.Size([self.batch_size])
+        numerator = torch.stack((pos_pairs, pos_pairs), dim=0).permute(1, 0).reshape(-1) # 2 * batchsize, with ordering [s12, s21, s34, s43, ...]
+        assert numerator.shape == torch.Size([2 * self.batch_size])
+        denom = torch.exp(torch.sum(self.sim_all_pairs(z1, z2), dim=-1) / self.temp) - torch.exp(1 / self.temp) # 2 * batch_size
+        assert denom.shape == torch.Size([2 * self.batch_size])
+        result = torch.sum(-torch.log(numerator / denom)) / 2 * self.batch_size
+        return result
 
