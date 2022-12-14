053f35579443346641ffe6d40505dde67f2a3649
Waltwang441 <31419755+Waltwang441@users.noreply.github.com>
2022-12-13 19:53:06 -0500

    delete/comment out unnecessary code

--- SimCLR_Loss.py
+++ SimCLR_Loss.py
@@ -44,32 +44,32 @@ class NTXent_Loss(nn.Module):
 
 
 
-class NTXent_Loss_updated(nn.Module):
-
-    def __init__(self, batch_size, temp):
-        super(NTXent_Loss_updated, self).__init__()
-        self.batch_size = batch_size
-        self.temp = temp
-
-    def forward(self, z1, z2):
-        z_mat = torch.stack((z1,z2), dim=1).view(z1.size()[0]*2,z1.size()[1])
-        sim_mat = F.cosine_similarity(z_mat[None, :, :], z_mat[:, None, :], dim=-1) / self.temp
-        exp_sim_mat = torch.exp(sim_mat)
-
-
-        sums = exp_sim_mat.sum(dim=1)
-        diagonal_entries = exp_sim_mat.diagonal()
-
-        sum_ = sums - diagonal_entries
-
-        pair_sim = torch.exp(F.cosine_similarity(z1, z2, dim=1)/self.temp)
-        pair_sim = pair_sim.repeat_interleave(2)
-
-        numdom = pair_sim / sum_
-
-        loss = -torch.log(numdom)
-
-        total_loss = loss.sum() / (2 * self.batch_size)
-        return total_loss
+# class NTXent_Loss_updated(nn.Module):
+#
+#     def __init__(self, batch_size, temp):
+#         super(NTXent_Loss_updated, self).__init__()
+#         self.batch_size = batch_size
+#         self.temp = temp
+#
+#     def forward(self, z1, z2):
+#         z_mat = torch.stack((z1,z2), dim=1).view(z1.size()[0]*2,z1.size()[1])
+#         sim_mat = F.cosine_similarity(z_mat[None, :, :], z_mat[:, None, :], dim=-1) / self.temp
+#         exp_sim_mat = torch.exp(sim_mat)
+#
+#
+#         sums = exp_sim_mat.sum(dim=1)
+#         diagonal_entries = exp_sim_mat.diagonal()
+#
+#         sum_ = sums - diagonal_entries
+#
+#         pair_sim = torch.exp(F.cosine_similarity(z1, z2, dim=1)/self.temp)
+#         pair_sim = pair_sim.repeat_interleave(2)
+#
+#         numdom = pair_sim / sum_
+#
+#         loss = -torch.log(numdom)
+#
+#         total_loss = loss.sum() / (2 * self.batch_size)
+#         return total_loss
 
     
